c5a38f563af9ea6c93334686758c31c23f7f02ec
Kristian Solkær Gade Sørensen <kristian.g.soerensen@hotmail.com>
2023-09-19 13:05:30 +0200

    [IMP] Command line args

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_assets import Environment, Bundle
 from views.home import home_blueprint
 import os
+import sys, getopt
 from assets_registry import js_files, scss_files
 
 def create_app_assets(app):
@@ -41,7 +42,21 @@ app = create_app()
 assets = create_app_assets(app)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    debug = bool(os.environ.get('DEBUG', False))
-    
-    app.run(debug=debug, host='0.0.0.0', port=port)
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv,"h:p:o:",["debug="])
+
+    host = "0.0.0.0"
+    port = 5000
+    debug = False
+
+    for opt, arg in opts:
+        if opt in ('-h', '-host'):
+            host = arg
+        elif opt in ('-p', '--port'):
+            port = int(arg)
+        elif opt == '--debug': 
+            debug = bool(arg)
+
+    assets.debug = debug
+    app.config['ASSETS_DEBUG'] = debug
+    app.run(debug=debug, host=host, port=port)
